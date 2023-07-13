54d12cc1eeddb25e90a087e8593fbde07a595d93
ra1der <73917011+baranraider@users.noreply.github.com>
2023-07-13 21:35:06 +0300

    Add files via upload

---
+++ bCarControl/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'bodacious'
+game 'gta5'
+name 'oxmysql'
+description 'Nopixel inspired car control | Created by Wild Dev'
+url 'discord.gg/wilddev'
+author 'FerZom (b.) | Wild Dev'
+
+client_scripts {
+  'client/client.lua'
+}
+
+ui_page 'ui/index.html'
+
+files {
+	'ui/index.html',
+	'ui/css/*.css',
+	'ui/js/*.js',
+	'ui/img/*.png',
+}
